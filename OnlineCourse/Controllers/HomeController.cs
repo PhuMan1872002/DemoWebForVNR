ba011b733b68e83e8cc15da9d890795e29be2844
phuman1872002@gmail.com <phuman1872002@gmail.com>
2024-10-23 20:05:56 +0700

    Web Demo For VNR

---
+++ OnlineCourse/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineCourse.Models;
+namespace OnlineCourse.Controllers
+{
+    public class HomeController : Controller
+    {
+        VNR_InternShipEntities db = new VNR_InternShipEntities();
+        public ActionResult Index()
+        {
+            List<MonHoc> monHoc = db.MonHocs.Where(s => s.KhoaHocID == 1).ToList();
+            return View(monHoc);
+        }
+        public JsonResult GetMonHocByKhoaHocId(int khoaHocId)
+        {
+            List<string> monHoc = db.MonHocs
+                                    .Where(m => m.KhoaHocID == khoaHocId)
+                                    .Select(m => m.TenMonHoc)  // Chỉ lấy tên môn học
+                                    .ToList();
+            return Json(monHoc, JsonRequestBehavior.AllowGet);
+        }
+
+      
+    }
+}
